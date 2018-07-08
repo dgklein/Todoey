9bee85dbed232ce999b14891da87d066066a3680
Dara Klein <daraklein@daras-macbook.local>
2018-07-08 09:33:06 -0500

    refactoring

--- Controllers/TodoListViewController.swift
+++ Controllers/TodoListViewController.swift
@@ -4,58 +4,79 @@
 //
 //  Created by Dara Klein on 7/5/18.
 //  Copyright © 2018 Dara Klein. All rights reserved.
-//
 
 import UIKit
-
 class TodoListViewController: UITableViewController {
-    var itemArray = ["lawyer", "Get Cheerios", "Call Bobby", "Pick up prescriptions", "Purified Water", "Bank", "Order Contacts"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+
+       let newItem1 = Item()
+        newItem1.title = "Find Mike"
+        newItem1.done = true
+        itemArray.append(newItem1)
+        
+        
+        let newItem2 = Item()
+        newItem2.title = "buy cheerios"
+        itemArray.append(newItem2)
+        
+        
+        let newItem3 = Item()
+        newItem3.title = "buy wine"
+        itemArray.append(newItem3)
     }
 //MARK - TableviewDataSource Methods
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellForRowAtindexPath called")
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        //to avoid such repetition
+        let item = itemArray[indexPath.row]
+
+        cell.textLabel?.text = item.title
+        
+        //ternary operator ==>
+        //value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
-      
     }
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(indexPath)
-        //print(indexPath.row)
-        //print(itemArray[indexPath.row])
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-       tableView.deselectRow(at: indexPath, animated: true)
+       
+    itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+       
+    tableView.reloadData()
+     
+    tableView.deselectRow(at: indexPath, animated: true)
 }
     
     //MARK - Add New Items
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         //initialize to empty UITextField Object
-        var textField = UITextField()
+        let textField = UITextField()
         
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 //what will happen once the user clicks the Add Item button on our UIAlert
-            self.itemArray.append(textField.text!)
-            //Persist Data locally - 
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+          //NOTE- broke after adding data model, added the two lines above
+            self.itemArray.append(newItem)
+        
+            //Persist Data locally -
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             //Reload Data onto TableView
             self.tableView.reloadData()
@@ -63,7 +84,7 @@ class TodoListViewController: UITableViewController {
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
-            textField = alertTextField
+            textField.text = (alertTextField.text)
             print("Not What you'd expect timewise, huh?")
         }
         
---
+++ Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Dara Klein on 7/7/18.
+//  Copyright © 2018 Dara Klein. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    //TODO:  Item object needs title and done
+    var title: String = ""
+    var done: Bool = false
+    
+}
